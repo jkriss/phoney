245d7c1d977eb23d8a22e2e0fdeaef05218e9949
Jesse Kriss <jesse@jklabs.net>
2010-03-04 20:49:50 -0800

    adding replies

--- phoney.rb
+++ phoney.rb
@@ -19,6 +19,12 @@ migration "create recordings table" do
   end
 end
 
+migration "add previous recording link" do
+  database.alter_table :recordings do
+    add_column :previous_recording_id, :string
+  end
+end
+
 get '/' do
   h = "<h1>#{database[:recordings].count} recordings</h1>"
   h += "<ul>"
@@ -31,19 +37,20 @@ end
 
 post '/voice' do
   content_type :xml
+  last_recording = database[:recordings].order(:created_at).last
   verb = Twilio::Verb.new { |v|
     v.say "Welcome to Die Ull A Story"
+    v.play last_recording[:url]
     v.say "At the beep, record the next line. Press star when you're finished"
-    v.record :finishOnKey => '*', :action => 'recording'
+    v.record :finishOnKey => '*', :action => "recording?in_reply_to=#{last_recording[:id]}"
     v.hangup
   }
   verb.response
 end
 
 post '/recording' do
-  @@logger.info params['RecordingUrl']
-  
-  database[:recordings].insert( :url => params['RecordingUrl'], :created_at => Time.now, :zip_code => params['CallerZip'])
+  @@logger.info params['RecordingUrl']  
+  database[:recordings].insert( :url => params['RecordingUrl'], :created_at => Time.now, :zip_code => params['CallerZip'], :previous_recording_id => params[:in_reply_to])
   # recording = request.body.read
   # @@logger.info recording.inspect
   # @@logger.info recording.class
